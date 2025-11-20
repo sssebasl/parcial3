54eeba2463f0c3dabd72d82a55d7c5c603a14dde
sssebasl <sebastianladino002@gmail.com>
2025-11-19 23:08:31 -0500

    Update Camion.java
    Replaced 'genero' with 'tipo' attribute and updated constructors and methods accordingly.

--- examen (1)/examen/src/main/java/co/edu/poli/parcial/model/Camion.java
+++ examen (1)/examen/src/main/java/co/edu/poli/parcial/model/Camion.java
@@ -2,72 +2,40 @@ package co.edu.poli.parcial.model;
 
 import java.io.Serializable;
 
-/**
- * Clase que representa una película dentro del sistema de producciones audiovisuales.
- * 
- * <p>Hereda de {@link Vehiculo} e implementa {@link Serializable}
- * para permitir su almacenamiento y recuperación desde archivos.</p>
- * 
- * <p>Incluye un atributo adicional para el género cinematográfico.</p>
- * 
- * @author Felipe Parra
- */
+
 public class Camion extends Vehiculo implements Serializable {
 
-    /** Género cinematográfico de la película */
-    private String genero;
 
-    /**
-     * Constructor por defecto.
-     * <p>Permite crear un objeto {@code Pelicula} sin inicializar sus atributos.</p>
-     */
+    private String tipo;
+
+ 
     public Camion() {}
 
-    /**
-     * Constructor que inicializa todos los atributos de la película.
-     * 
-     * @param placa código único de la producción
-     * @param marca título de la película
-     * @param fechaEstreno año de estreno de la película
-     * @param capacidad duración en minutos
-     * @param director objeto {@link Conductor} asociado a la película
-     * @param genero género cinematográfico de la película
-     */
-    public Camion(String placa, String marca, int fechaEstreno, int capacidad, Conductor director, String genero) {
-        super(placa, marca, fechaEstreno, capacidad, director);
+ 
+    public Camion(String placa, String marca, int fechaManofactura, int capacidad, Conductor conductor, String tipo) {
+        super(placa, marca, fechaManofactura, capacidad, conductor);
         this.genero = genero;
     }
 
-    /**
-     * Obtiene el género de la película.
-     * 
-     * @return género cinematográfico
-     */
-    public String getGenero() {
-        return genero;
+  
+    public String getTipo() {
+        return tipo;
     }
 
-    /**
-     * Asigna un nuevo género a la película.
-     * 
-     * @param genero nuevo género cinematográfico
-     */
-    public void setGenero(String genero) {
-        this.genero = genero;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
-    /**
-     * Muestra la información completa de la película en consola.
-     * <p>Sobrescribe el método {@link Vehiculo#mostrarInformacion()}.</p>
-     */
+
     @Override
     public void mostrarInformacion() {
-        System.out.println("\n--- Película ---");
+        System.out.println("\n--- Camion ---");
         System.out.println("Placa: " + getPlaca());
         System.out.println("Marca : " + getMarca());
-        System.out.println("Fecha de estreno: " + getFechaEstreno());
+        System.out.println("Fecha de manofacturacion: " + getFechaManofactura());
         System.out.println("Capacidad: " + getCapacidadMin() + " kilogramos ");
         System.out.println("Conductor: " + getConductor().getNombre());
-        System.out.println("Género: " + genero);
+        System.out.println("Tipo: " + tipo);
     }
+
 }
